4930ffbabdccb937738b90349d88684149128d9e
Suhas Somnath <14300780+ssomnath@users.noreply.github.com>
2020-03-07 13:57:01 -0500

    Added options to run benchmarks for pandas, cudf
    Added options to run groupby benchmarks for all sizes if 'all' is specified

--- gtc_2020/cudf/groupby.py
+++ gtc_2020/cudf/groupby.py
@@ -40,7 +40,8 @@ def generate_timeseries(num_ids=1000, freq='1d', seed=1, csv_path=None, **kwargs
         
     return dask_df
 
-def generate_data(targ_size, num_ids=1000):
+def get_frequency(targ_size):
+    
     freq_mapper = {'1G': '163s',
                '2.5G': '65s',
                '5G': '33s',
@@ -54,10 +55,19 @@ def generate_data(targ_size, num_ids=1000):
                '2.5T': '65ms',
                '5T': '33ms',
                '10T': '16ms'}
+    
+    return freq_mapper[targ_size]
 
+def get_timeseries_path(freq, num_ids=1000):
+    
+    return '/gpfs/alpine/world-shared/stf011/somnaths/rapids/data/timeseries_{}_ids_{}_freq.csv'.format(num_ids, freq)
+
+def generate_data(targ_size, num_ids=1000):
+    
     #num_ids = 1000 # Not sure if this number should be changed
-    freq = freq_mapper[targ_size]
-    csv_path = '/gpfs/alpine/world-shared/stf011/somnaths/rapids/data/timeseries_{}_ids_{}_freq.csv'.format(num_ids, freq)
+    freq = get_frequency(targ_size)
+    
+    csv_path = get_timeseries_path(freq, num_ids)
 
     if os.path.exists(csv_path):
         print('Reading existing csv file')
@@ -66,100 +76,103 @@ def generate_data(targ_size, num_ids=1000):
         _ = generate_timeseries(num_ids=num_ids, freq=freq, csv_path=csv_path)
     return csv_path
 
-"""
-'1day' = 365
-'1hour' = 8784 or 8.8E+3
------ WITH 261 COLUMNS ---------
-'3min' = 1.75680E+5 = 0.9 GB  -> 163 sec for 1 GB
-'1min' = 5.27040E+5 = 2.7 GB -> 65sec for 2.5 GB
-'30s' = 1.054,080E+6 <----------- 5.41 GB. Use 
-'15s' = ~ ?? <----------- ~ 10 GB Use 16.23
-'6s' = ~ ?? <----------- ~ 25 GB
-'3s' = ~ 10M <----------- ~ 50 GB
-'1500ms' = ????? <----------- ~ 100 GB
-???? = ????? <----------- ~ 250 GB
-???? = ????? <----------- ~ 500 GB
-???? = ????? <----------- ~ 1.0 TB
-???? = ????? <----------- ~ 2.5 TB
-???? = ????? <----------- ~ 5.0 TB
-'1sec' = 3.162E+7
 
-milliseconds = 3.162E+10
-
-single GPU benchmarked up to 1E+8 rows but unknown number of columns
-"""
-
-def benchmark_groupby(hardware, targ_size):
+def benchmark_groupby(package_name, targ_size):
+    """
+    single GPU benchmarked up to 1E+8 rows but unknown number of columns
+    """
     if not isinstance(targ_size, str):
         raise TypeError('targ_size should be of type str. Provided object ({}) of type ({})'.format(targ_size, type(targ_size)))
-    if not isinstance(hardware, str):
-        raise TypeError('hardware should be of type str. Provided object ({}) of type ({})'.format(hardware, type(hardware)))
+    if not isinstance(package_name, str):
+        raise TypeError('package_name should be of type str. Provided object ({}) of type ({})'.format(package_name, type(package_name)))
     
     print('\n'*2 + '~'*10 + ' RAPIDS BENCHMARKING - GROUPBY START ' + '~'*10)
-    print('Target dataframe size: {}. Hardware: {}\n'.format(targ_size, hardware))
+    print('Target dataframe size: {}. package_name: {}\n'.format(targ_size, package_name))
     
-    if hardware == 'gpu':
-        package_name = 'dask-cudf'
-        package_handle = dask_cudf
-        scheduler_json_name = 'my-scheduler-gpu.json'
-    elif hardware == 'cpu':
-        package_name = 'dask'
-        package_handle = dd
-        scheduler_json_name = 'my-scheduler.json'
+    scheduler_json_name = None
+    
+    dframe_package_dict = {'dask-cudf': (dask_cudf, 'my-scheduler-gpu.json'),
+                           'dask': (dd, 'my-scheduler.json'),
+                           'cudf': (cudf, None),
+                           'pandas': (pd, None),
+                          }
+    
+    
+    package_handle, scheduler_json_name = dframe_package_dict[package_name]
         
     # -------------- SET UP CLUSTER --------------------------------
 
-    file = os.getenv('MEMBERWORK') + '/stf011/' + scheduler_json_name 
-    client = Client(scheduler_file=file)
-    print("client information {}".format(client))
+    if scheduler_json_name is not None:
+        file = os.getenv('MEMBERWORK') + '/stf011/' + scheduler_json_name 
+        client = Client(scheduler_file=file)
+        print("client information {}".format(client))
 
     # ----------------- GENERATE RANDOM DATA ------------------------
-    csv_path = generate_data(targ_size, num_ids=1000)
+    csv_path = get_timeseries_path(get_frequency(targ_size), num_ids=1000)
 
     # ----------------- READ FROM FILE -----------------------------
     t0 = time.time()
     t_start = t0
-    dc_df = package_handle.read_csv('file://' + csv_path).persist()
+    dframe = package_handle.read_csv('file://' + csv_patht)
+    if scheduler_json_name is not None:
+        dframe = dframe.persist()
     print('Time to load a {} file with {}: {}'.format(format_bytes(os.stat(csv_path).st_size), package_name, format_time(time.time() - t0)))
 
-    print('Dataframe object of type: {}'.format(type(dc_df)))
+    print('Dataframe object of type: {}'.format(type(dframe)))
 
     # ----------------- SEE HEAD -----------------------------
 
-    # print(dc_df.head().to_pandas())
+    # print(dframe.head().to_pandas())
 
     # -------------- QUERY SIZE OF DATAFRAME ------------------------
 
     t0 = time.time()
-    print('Dataframe has {} rows and {} columns'.format(len(dc_df), len(dc_df.columns)))
+    print('Dataframe has {} rows and {} columns'.format(len(dframe), len(dframe.columns)))
     print('Time to calculate number of rows using {}: {}'.format(package_name, format_time(time.time() - t0)))
 
     # -------------- COMPUTE UNIQUE IDs ------------------------
 
     t0 = time.time()
-    uniq_ids = dc_df.id.nunique().compute()
+    uniq_ids = dframe.id.nunique()
+    if scheduler_json_name is not None:
+        uniq_ids = uniq_ids.compute()
     print('Time to compute unique values: {} using {}: {}'.format(uniq_ids, package_name, format_time(time.time() - t0)))
 
     # -------------- COMPUTE GROUPBY ------------------------
 
     t0 = time.time()
-    vals = dask.compute(
-        dc_df.groupby('id').min(),
-        dc_df.groupby('id').max(),
-        dc_df.groupby('id').mean(),
-        dc_df.groupby('id').count(),
-    )
+    if scheduler_json_name is not None:
+        vals = dask.compute(
+            dframe.groupby('id').min(),
+            dframe.groupby('id').max(),
+            dframe.groupby('id').mean(),
+            dframe.groupby('id').count(),
+        )
+    else:
+        vals = (
+            dframe.groupby('id').min(),
+            dframe.groupby('id').max(),
+            dframe.groupby('id').mean(),
+            dframe.groupby('id').count(),
+        )
 
     print('Time to compute Groupby using {}: {}'.format(package_name, format_time(time.time() - t0)))
 
     # -------------- REPORT ------------------------
+    if scheduler_json_name is not None:
+        _ = client.profile(start=t_start, filename=package_name + '-groupby-aggregations-profile_'+ targ_size + '.html')
     
-        
-    _ = client.profile(start=t_start, filename=package_name + '-groupby-aggregations-profile_'+ targ_size + '.html')
     print('~'*10 + ' RAPIDS BENCHMARKING - GROUPBY END ' + '~'*10 + '\n'*2)
+    
+    # -------------- END ------------------------
     
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print('please run as "python <script_name>.py <mode> <size>"\n\t<mode> = gpu or cpu\n\t<size> = 1G, 2.5G, 5G, ....')
+        print('please run as "python <script_name>.py <package> <size>"\n\t<package> = dask, dask-cudf, cudf or pandas\n\t<size> = 1G, 2.5G, 5G, ...25G')
     else:
-        benchmark_groupby(sys.argv[1], sys.argv[2])
+        if sys.argv[2] == 'all':
+            for dsize in ['1G', '2.5G', '5G' , '10G', '25G']:
+                benchmark_groupby(sys.argv[1], dsize)
+        else:
+            benchmark_groupby(sys.argv[1], sys.argv[2])
+
