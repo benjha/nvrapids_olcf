0c9668a12dba1f7bc81b631567cc3483af653406
Suhas Somnath <14300780+ssomnath@users.noreply.github.com>
2020-03-11 12:45:28 -0500

    Added indexed join capability
    Tested simple cases on login node

--- gtc_2020/cudf/groupby.py
+++ gtc_2020/cudf/groupby.py
@@ -1,4 +1,3 @@
-# Groupby Aggregations with Dask and cuDF
 import sys
 import os
 import time
@@ -40,28 +39,6 @@ def generate_timeseries(num_ids=1000, freq='1d', seed=1, csv_path=None, **kwargs
         
     return dask_df
 
-def get_frequency(targ_size):
-    
-    freq_mapper = {'1G': '163s',
-               '2.5G': '65s',
-               '5G': '33s',
-               '10G': '16s',
-               '25G': '6500ms',
-               '50G': '3260ms',
-               '100G': '1630ms',
-               '250G': '652ms',
-               '500G': '326ms',
-               '1T': '163ms',
-               '2.5T': '65ms',
-               '5T': '33ms',
-               '10T': '16ms'}
-    
-    return freq_mapper[targ_size]
-
-def get_timeseries_path(freq, num_ids=1000):
-    
-    return '/gpfs/alpine/world-shared/stf011/somnaths/rapids/data/timeseries_{}_ids_{}_freq.csv'.format(num_ids, freq)
-
 def generate_data(targ_size, num_ids=1000):
     
     #num_ids = 1000 # Not sure if this number should be changed
@@ -76,50 +53,134 @@ def generate_data(targ_size, num_ids=1000):
         _ = generate_timeseries(num_ids=num_ids, freq=freq, csv_path=csv_path)
     return csv_path
 
+def get_frequency(targ_size):
+    
+    size_2_freq = {'1G': '163s',
+               '2.5G': '65s',
+               '5G': '33s',
+               '10G': '16s',
+               '25G': '6500ms',
+               '50G': '3260ms',
+               '100G': '1630ms',
+               '250G': '652ms',
+               '500G': '326ms',
+               '1T': '163ms',
+               '2.5T': '65ms',
+               '5T': '33ms',
+               '10T': '16ms'}
+    
+    return size_2_freq[targ_size]
 
-def benchmark_groupby(package_name, targ_size):
-    """
-    single GPU benchmarked up to 1E+8 rows but unknown number of columns
-    """
+def get_timeseries_path(freq, num_ids=1000):
+    
+    return '/gpfs/alpine/world-shared/stf011/somnaths/rapids/data/timeseries_{}_ids_{}_freq.csv'.format(num_ids, freq)
+
+def validate_inputs(package_name, targ_size):
+
     if not isinstance(targ_size, str):
         raise TypeError('targ_size should be of type str. Provided object ({}) of type ({})'.format(targ_size, type(targ_size)))
+    if targ_size not in ['1G', '2.5G', '5G' , '10G', '25G', 'all']: 
+        raise ValueError('Requested data size: "' +  targ_size + '" is not valid')
+        
     if not isinstance(package_name, str):
         raise TypeError('package_name should be of type str. Provided object ({}) of type ({})'.format(package_name, type(package_name)))
-    
-    print('\n'*2 + '~'*10 + ' RAPIDS BENCHMARKING - GROUPBY START ' + '~'*10)
-    print('Target dataframe size: {}. package_name: {}\n'.format(targ_size, package_name))
-    
-    scheduler_json_name = None
-    
-    dframe_package_dict = {'dask-cudf': (dask_cudf, 'my-scheduler-gpu.json'),
+    if package_name not in ['dask-cudf', 'dask', 'cudf', 'pandas']:
+        raise ValueError('Requested package: "' +  package_name + '" is not valid')
+        
+def get_package_scheduler(package_name):
+        
+    package_dict = {'dask-cudf': (dask_cudf, 'my-scheduler-gpu.json'),
                            'dask': (dd, 'my-scheduler.json'),
                            'cudf': (cudf, None),
                            'pandas': (pd, None),
                           }
     
-    
-    package_handle, scheduler_json_name = dframe_package_dict[package_name]
-        
-    # -------------- SET UP CLUSTER --------------------------------
+    return package_dict[package_name]
 
+def set_up_dask_client(scheduler_json_name):
+    client = None
     if scheduler_json_name is not None:
-        file = os.getenv('MEMBERWORK') + '/stf011/' + scheduler_json_name 
-        client = Client(scheduler_file=file)
+        scheduler_file = os.getenv('MEMBERWORK') + '/stf011/' + scheduler_json_name 
+        client = Client(scheduler_file=scheduler_file)
         print("client information {}".format(client))
-
-    # ----------------- GENERATE RANDOM DATA ------------------------
-    csv_path = get_timeseries_path(get_frequency(targ_size), num_ids=1000)
-
-    # ----------------- READ FROM FILE -----------------------------
+    return client
+        
+def read_csv(csv_path, package_handle, package_name):
     t0 = time.time()
-    t_start = t0
-    dframe = package_handle.read_csv('file://' + csv_patht)
-    if scheduler_json_name is not None:
+    dframe = package_handle.read_csv('file://' + csv_path)
+    if package_name in ['dask', 'dask-cudf']:
         dframe = dframe.persist()
     print('Time to load a {} file with {}: {}'.format(format_bytes(os.stat(csv_path).st_size), package_name, format_time(time.time() - t0)))
 
     print('Dataframe object of type: {}'.format(type(dframe)))
+    
+    return dframe
 
+# -------------- JOIN INDEXED --------------------------------
+
+def benchmark_join_indexed(package_name, targ_size):
+    
+    package_handle, scheduler_json_name = get_package_scheduler(package_name)
+    
+    print('\n'*2 + '~'*10 + ' RAPIDS BENCHMARKING - JOIN INDEXED START ' + '~'*10)
+    print('Target dataframe size: {}. package_name: {}\n'.format(targ_size, package_name))
+    
+    # -------------- SET UP CLUSTER --------------------------------
+    client = set_up_dask_client(scheduler_json_name)
+    
+    # --------------- GET RANDOM DATA FILE PATH---------------------
+    csv_left = get_timeseries_path(get_frequency(targ_size), num_ids=1000)
+    csv_right = get_timeseries_path(get_frequency(targ_size), num_ids=1001)
+
+    # ----------------- READ FROM FILE -----------------------------
+    t_start = time.time()
+    
+    dframe_left = read_csv(csv_left, package_handle, package_name)
+    dframe_right = read_csv(csv_right, package_handle, package_name)
+    
+    # cudf doesn't handle datetime indexes well yet, so we convert to integer dtype
+    dframe_left.index = dframe_left.index.astype(int)
+    dframe_right.index = dframe_right.index.astype(int)
+    
+    # ----------------- PERFORM THE MERGE -----------------------------
+    t0 = time.time()
+    dframe_out = dframe_left.merge(dframe_right, left_index=True, right_index=True, how='inner')
+    if scheduler_json_name is not None:
+        dframe_out = dframe_out.persist()
+        _ = wait(dframe_out)
+    print('Time to perform merge using {}: {}'.format(package_name, format_time(time.time() - t0)))
+    
+    # -------------- REPORT ------------------------
+    if scheduler_json_name is not None:
+        _ = client.profile(start=t_start, filename=package_name + '-join-indexed-profile_'+ targ_size + '.html')
+    
+    print('\n'*2 + '~'*10 + ' RAPIDS BENCHMARKING - JOIN INDEXED END ' + '~'*10)
+    
+# -------------- GROUPBY --------------------------------
+
+def benchmark_groupby(package_name, targ_size):
+    """
+    single GPU benchmarked up to 1E+8 rows but unknown number of columns
+    """    
+    package_handle, scheduler_json_name = get_package_scheduler(package_name)
+    
+    print('\n'*2 + '~'*10 + ' RAPIDS BENCHMARKING - GROUPBY START ' + '~'*10)
+    print('Target dataframe size: {}. package_name: {}\n'.format(targ_size, package_name))
+        
+    # -------------- SET UP CLUSTER --------------------------------
+    client = set_up_dask_client(scheduler_json_name)
+    
+    # --------------- GET RANDOM DATA FILE PATH---------------------
+    csv_path = get_timeseries_path(get_frequency(targ_size), num_ids=1000)
+
+    # ----------------- READ FROM FILE -----------------------------
+    t_start = time.time()
+    
+    dframe = read_csv(csv_path, package_handle, package_name)
+
+    # Repartition to maximize performance
+    #dframe = dframe.repartition(npartitions=dframe.npartitions // 100)
+    
     # ----------------- SEE HEAD -----------------------------
 
     # print(dframe.head().to_pandas())
@@ -167,12 +228,20 @@ def benchmark_groupby(package_name, targ_size):
     # -------------- END ------------------------
     
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('please run as "python <script_name>.py <package> <size>"\n\t<package> = dask, dask-cudf, cudf or pandas\n\t<size> = 1G, 2.5G, 5G, ...25G')
+    if len(sys.argv) != 4:
+        print('please run as "python <script_name>.py <task> <package> <size>"\n\t<task> = groupby or join-indexed\n\t<package> = dask, dask-cudf, cudf or pandas\n\t<size> = 1G, 2.5G, 5G, ...25G')
     else:
-        if sys.argv[2] == 'all':
+        if sys.argv[1] not in ['groupby', 'join-indexed']:
+            raise ValueError('First argument should either be "groupby" or "join-indexed"')
+        if sys.argv[1] == 'groupby':
+            benchmark_func = benchmark_groupby
+        elif sys.argv[1] == 'join-indexed':
+            benchmark_func = benchmark_join_indexed
+        
+        validate_inputs(sys.argv[2], sys.argv[3])
+        
+        if sys.argv[3] == 'all':
             for dsize in ['1G', '2.5G', '5G' , '10G', '25G']:
-                benchmark_groupby(sys.argv[1], dsize)
+                benchmark_func(sys.argv[2], dsize)
         else:
-            benchmark_groupby(sys.argv[1], sys.argv[2])
-
+            benchmark_func(sys.argv[2], sys.argv[3])
