00dbb9816e945087f54e973a6e78c849b4106dab
Mark Coletti <colettima@ornl.gov>
2020-04-09 16:31:49 -0400

    Added max time to wait

--- dask-bulk-batch/wait_for_workers.py
+++ dask-bulk-batch/wait_for_workers.py
@@ -1,40 +1,21 @@
 #!/usr/bin/env python3
 """
-This utility will block on the scheduler until a specified number of workers
+This utility will block on the scheduler until a certain number of workers
 have registered.  This is useful for summit batch jobs to block in LSF scripts
 until all the workers are spun up for work.
-
-Mark Coletti
-colettima@ornl.gov
-Oak Ridge National Laboratory
-
-
-usage: wait_for_workers.py [-h] [--target-workers TARGET_WORKERS]
-                           [--pause-time PAUSE_TIME] [--verbose]
-                           scheduler_file
-
-dask utility for blocking until set target of workers is registered
-
-positional arguments:
-  scheduler_file        Scheduler file for the running scheduler
-
-optional arguments:
-  -h, --help            show this help message and exit
-  --target-workers TARGET_WORKERS, -t TARGET_WORKERS
-                        The target total number of workers on which to wait;
-                        if not specified, just dump out the worker count.
-  --pause-time PAUSE_TIME, -p PAUSE_TIME
-                        How long to pause between polling scheduler for number
-                        of workers
-  --verbose, -v         Verbose output
 """
+import time
 import argparse
 import time
+import sys
 
 from distributed import Client
 
 # How many seconds to sleep between polling scheduler for number of workers
 DEFAULT_SLEEP = 5
+
+# How many seconds should we wait on the dask scheduler?
+DEFAULT_TIMEOUT = 30
 
 
 def get_num_workers(client):
@@ -59,6 +40,12 @@ if __name__ == '__main__':
     parser.add_argument('--pause-time', '-p', type=int, default=DEFAULT_SLEEP,
                         help='How long to pause between polling scheduler for '
                              'number of workers')
+    parser.add_argument('--scheduler-timeout', type=int,
+                        default=DEFAULT_TIMEOUT,
+                        help='How long to wait on the scheduler')
+    parser.add_argument('--maximum-wait-time', type=int,
+                        help='Optional parameter for maximum amount of time '
+                             'to wait in minutes before proceeding')
     parser.add_argument('--verbose', '-v', action='store_true',
                         help='Verbose output')
     parser.add_argument('scheduler_file',
@@ -66,24 +53,57 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    # Track start time if we have a secondary termination criteria of a dead-
+    # line.
+    if args.maximum_wait_time is not None and args.verbose:
+        print(f'Deadline set for {args.maximum_wait_time} minutes')
+
     if args.verbose:
         print(f'Target workers: {args.target_workers}')
+        print(f'Scheduler timeout: {args.scheduler_timeout} secs')
+        print(f'Pause time: {args.pause_time} secs')
         print(f'Scheduler file: {args.scheduler_file}')
 
-    with Client(scheduler_file=args.scheduler_file) as client:
+    try:
+        with Client(scheduler_file=args.scheduler_file,
+                    timeout=args.scheduler_timeout) as client:
 
-        if args.target_workers is None:
-            print(f'{get_num_workers(client)} workers')
-        else:
-            curr_num_workers = 0
+            if args.target_workers is None:
+                print(f'{get_num_workers(client)} workers')
+                # Now exit since this means we just wanted the current total
+                # number of workers, and didn't want to wait for a target
+                # set number of workers, after all.
+            else:
+                start_time = time.time()
 
-            start_time = time.time()
+                if args.verbose:
+                    print('seconds\tnumber of workers')
 
-            while curr_num_workers < args.target_workers:
+                prev_curr_num_workers = 0
                 curr_num_workers = get_num_workers(client)
 
-                time.sleep(args.pause_time)
+                while curr_num_workers < args.target_workers:
 
-            if args.verbose:
-                print(f'{time.time() - start_time} seconds to register '
-                      f'{curr_num_workers} workers')
+                    if args.maximum_wait_time is not None and \
+                            args.maximum_wait_time < (time.time() - start_time) / 60:
+                        print(f'Maximum time of {args.maximum_wait_time} minute met.  Exiting.')
+                        break
+
+                    if args.verbose and prev_curr_num_workers != curr_num_workers:
+                        # Just echo the time in seconds since we started and
+                        # the current number of workers so we get a sense of how
+                        # quickly workers are being added over time
+                        print(
+                            f'{time.time() - start_time:.2f}\t{curr_num_workers}')
+
+                    time.sleep(args.pause_time)
+
+                    prev_curr_num_workers = curr_num_workers
+                    curr_num_workers = get_num_workers(client)
+
+                if args.verbose:
+                    print(f'{time.time() - start_time:.2f} seconds to register '
+                          f'{curr_num_workers} workers')
+    except Exception as e:
+        print(f'Raised {e!r} during execution')
+        sys.exit(1)
